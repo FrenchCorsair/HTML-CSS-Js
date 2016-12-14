7cf292d62242d378c0ffdfee0bd482af515966de
FrenchCorsair <fchagny@gmail.com>
2016-12-14 18:02:21 +0100

    Les trois boutons sont en place
    Mais un seul me sert à peaufiner les effets

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 $(function() {
   // Ici, le DOM est entièrement défini
-  $("#bouton").click(function(){
+  $("#bouton1").mouseenter(function(){
+      $(this).css("background-color","#4A78E0");
+    }); 
+  $("#bouton1").mouseleave(function(){
+      $(this).css("background-color","#C39DD5");
+    }); 
+  $("#bouton1").click(function(){
       $(this).html("Et voilà!").css("background-color","#FE0A24");
     }); 
 });
