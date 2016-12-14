2d38df65aa10d76fa8f8d6d2ec454f6834f03976
FrenchCorsair <fchagny@gmail.com>
2016-12-14 16:02:37 +0100

    Modif du comportement Jquery
    Le bouton comporte deux instructions Jquery

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 $(function() {
-
   // Ici, le DOM est entièrement défini
-  $(".bouton").click(function(){
-
-      $(this).html("Et voilà!");
-
+  $("#bouton").click(function(){
+      $(this).html("Et voilà!").css("background-color","#FE0A24");
     }); 
 });
