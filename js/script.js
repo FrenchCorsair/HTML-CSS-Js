750f936817c36f976eb0ae45681149aed369f2e3
FrenchCorsair <fchagny@gmail.com>
2016-12-15 16:05:48 +0100

    Réussite des boutons qui révèlent un autre bouton
    Pas mal de progrès par tatonnement, je n'ai pas encore bien compris
    comment j'y suis arrivé mais la documentation Jquery est bien utile!

--- js/script.js
+++ js/script.js
@@ -8,5 +8,16 @@ $(function() {
     }); 
   $("#bouton1").click(function(){
       $(this).html("Et voilà!").css("background-color","#FE0A24");
+      $("#bouton2").show("#bouton2");
+    }); 
+  $("#bouton2").mouseenter(function(){
+      $(this).css("background-color","#4A78E0");
+    }); 
+  $("#bouton2").mouseleave(function(){
+      $(this).css("background-color","#C39DD5");
+    }); 
+  $("#bouton2").click(function(){
+      $(this).html("Et voilà!").css("background-color","#FE0A24");
+      $("#bouton3").show("#bouton3");
     }); 
 });
