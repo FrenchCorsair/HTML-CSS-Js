4f68ed32b90aef3bc85eda2bdd251fd07c9ded45
FrenchCorsair <fchagny@gmail.com>
2016-12-17 17:17:29 +0100

    L'essentiel est terminé
    Je crois que j'ai compris toutes les phases de l'exercice concernant
    Jquery. Je vais pouvoir peaufiner sans difficulté et passer à la suite.

--- js/script.js
+++ js/script.js
@@ -17,7 +17,27 @@ $(function() {
       $(this).css("background-color","#C39DD5");
     }); 
   $("#bouton2").click(function(){
-      $(this).html("Et voilà!").css("background-color","#FE0A24");
+      $(this).html("<audio src='toc.mp3' autoplay></audio>Et voilà!").css("background-color","#FE0A24");
       $("#bouton3").show("#bouton3");
+    }); 
+   $("#bouton3").mouseenter(function(){
+      $(this).css("background-color","#4A78E0");
+    }); 
+  $("#bouton3").mouseleave(function(){
+      $(this).css("background-color","#C39DD5");
+    }); 
+  $("#bouton3").click(function(){
+      $(this).html("<audio src='toc.mp3' autoplay></audio>Et voilà!").css("background-color","#FE0A24");
+      $("#bouton4").show("#bouton4");
+    }); 
+  $("#bouton4").mouseenter(function(){
+      $(this).css("background-color","#4A78E0");
+    }); 
+  $("#bouton4").mouseleave(function(){
+      $(this).css("background-color","#C39DD5");
+    }); 
+  $("#bouton4").click(function(){
+      $(this).html("<audio src='toc.mp3' autoplay></audio>Et voilà!").css("background-color","#FE0A24");
+      $("body").css("background-color", "pink");
     }); 
 });
